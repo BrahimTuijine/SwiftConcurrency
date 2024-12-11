a2da18f0e4fecbdd86b256485d7c54e16e758e27
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-11 11:01:58 +0100

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by MacBook on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
