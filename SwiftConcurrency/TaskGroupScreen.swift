5f5865978ea3806d91e56520601d25d2a6c7a3f0
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-14 00:22:35 +0100

    fetch Images With TaskGroup

--- SwiftConcurrency/TaskGroupScreen.swift
+++ SwiftConcurrency/TaskGroupScreen.swift
@@ -13,10 +13,27 @@ class TaskGroupDataManager {
         async let fetchImage1 = fetchImage()
         async let fetchImage2 = fetchImage()
         async let fetchImage3 = fetchImage()
+        async let fetchImage4 = fetchImage()
         
-        let (image1, image2, image3) = await (try fetchImage1,try fetchImage2,try fetchImage3)
+        let (image1, image2, image3, image4) = await (try fetchImage1,try fetchImage2,try fetchImage3, try fetchImage4)
         
-        return [image1, image2, image3]
+        return [image1, image2, image3, image4]
+    }
+    
+    func fetchImagesWithTaskGroup() async throws -> [UIImage] {
+        try await withThrowingTaskGroup(of: UIImage.self) { group in
+            var images: [UIImage] = []
+            
+            for _ in (0..<10) {
+                group.addTask { try await self.fetchImage() }
+            }
+            
+            for try await image in group {
+                images.append(image)
+            }
+            
+            return images
+        }
     }
     
     private func fetchImage() async throws -> UIImage {
@@ -42,6 +59,13 @@ class TaskGroupDataManager {
 class TaskGroupViewModel: ObservableObject {
     @Published var images: [UIImage] = []
     let manager = TaskGroupDataManager()
+    
+    func fetchImages() async -> Void {
+        let images = try? await manager.fetchImagesWithTaskGroup()
+        if let images = images {
+            self.images = images
+        }
+    }
 }
 
 struct TaskGroupScreen: View {
@@ -50,17 +74,22 @@ struct TaskGroupScreen: View {
     let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns) {
-                ForEach(vm.images, id: \.self) { image in
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 150)
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(vm.images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
                 }
             }
+            .navigationTitle("Task Group 🥳")
+            .task {
+                await vm.fetchImages()
         }
-        .navigationTitle("Task Group 🥳")
+        }
     }
 }
 
