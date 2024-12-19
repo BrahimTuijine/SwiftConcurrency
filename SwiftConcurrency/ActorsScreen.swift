9e78b5f2d81c3258511e5c96c6d71392d795b40f
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-20 00:08:12 +0100

    make class thread safe using custom DispatchQueue (old way)

--- SwiftConcurrency/ActorsScreen.swift
+++ SwiftConcurrency/ActorsScreen.swift
@@ -7,14 +7,89 @@
 
 import SwiftUI
 
+class MyDataManager {
+    
+    static let instance = MyDataManager()
+    private init() {}
+    
+    var data: [String] = []
+    
+    private let lock = DispatchQueue(label: "com.MyApp.MyDataManager")
+    
+    func getRandomData(completionHandler: @escaping (_ title: String?) -> ()) {
+        lock.async {
+            self.data.append(UUID().uuidString)
+            print(Thread.current.isMainThread)
+            completionHandler(self.data.randomElement())
+        }
+    }
+}
+
+struct HomeView: View {
+    
+    let dataManager = MyDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.8).ignoresSafeArea()
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer, perform: {  _ in
+            DispatchQueue.global(qos: .background).async {
+                dataManager.getRandomData { title in
+                    if let data = title {
+                        DispatchQueue.main.async {
+                            self.text = data
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
+
+struct BrowseView: View {
+    
+    let dataManager = MyDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            
+            Color.yellow.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+            
+        }
+        .onReceive(timer, perform: {  _ in
+            DispatchQueue.global(qos: .default).async {
+                dataManager.getRandomData { title in
+                    if let data = title {
+                        DispatchQueue.main.async {
+                            self.text = data
+                        }
+                    }
+                }
+            }
+        })
+    }
+    
+}
+
 struct ActorsScreen: View {
+    
     var body: some View {
         TabView {
-            Text("hi")
+            HomeView()
                 .tabItem {
                     Label("Home", systemImage: "house.fill")
                 }
-            Text("hi")
+            BrowseView()
                 .tabItem {
                     Label("Browse", systemImage: "magnifyingglass")
                 }
