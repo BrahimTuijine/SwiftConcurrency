c2bfef7da45511f0403dca9483e494b9b0a89704
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-22 00:00:05 +0100

    use AsyncPublisher(.values) to convert @Published to Async / Await

---
+++ SwiftConcurrency/AsyncPublisherScreen.swift
@@ -0,0 +1,84 @@
+//
+//  AsyncPublisherScreen.swift
+//  SwiftConcurrency
+//
+//  Created by MacBook on 21/12/2024.
+//
+
+import SwiftUI
+import Combine
+
+class AsyncPublisherDataManager {
+    
+    @Published var data: [String] = []
+    
+    func addData() async {
+        data.append("apple")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        data.append("banana")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        data.append("orange")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        data.append("watermelon")
+    }
+}
+
+class AsyncPublisherViewModel: ObservableObject {
+    
+    @MainActor @Published var dataArray: [String] = []
+    let manager = AsyncPublisherDataManager()
+    var cancellables = Set<AnyCancellable>()
+    
+    init()  {
+         addSubscribers()
+    }
+    
+    func addSubscribers() -> Void {
+        
+        Task {
+            for await value in manager.$data.values {
+                await MainActor.run {
+                    self.dataArray = value
+                }
+            }
+        }
+        
+//        Task {
+//            for await value in manager.$data.values {
+//                await MainActor.run {
+//                    self.dataArray = value
+//                }
+//            }
+//        }
+        
+        //        manager.$data.sink { data in
+        //            self.dataArray = data
+        //        }
+        //        .store(in: &cancellables)
+    }
+    
+    func start() async {
+        await manager.addData()
+    }
+}
+
+struct AsyncPublisherScreen: View {
+    
+    @StateObject private var vm = AsyncPublisherViewModel()
+    
+    var body: some View {
+        ScrollView {
+            ForEach(vm.dataArray, id: \.self) {
+                Text($0)
+                    .font(.headline)
+            }
+        }
+        .task {
+            await vm.start()
+        }
+    }
+}
+
+#Preview {
+    AsyncPublisherScreen()
+}
