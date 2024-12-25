678e212af9dfae5c0887c50b7a565afd437d118c
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-25 19:11:38 +0100

    create Observable Macro viewModel

---
+++ SwiftConcurrency/ObservableMacroScreen.swift
@@ -0,0 +1,42 @@
+//
+//  ObservableMacroScreen.swift
+//  SwiftConcurrency
+//
+//  Created by MacBook on 25/12/2024.
+//
+
+import SwiftUI
+
+actor TitleDataBase {
+    func getTitle() -> String {
+        "title from db"
+    }
+}
+
+@Observable class ObservableMacroViewModel {
+    @MainActor var title: String = "starting text !"
+    @ObservationIgnored let db = TitleDataBase()
+    
+    func updateTitle() async -> Void {
+        let msg = await db.getTitle()
+        await MainActor.run {
+            title = msg
+        }
+    }
+}
+
+struct ObservableMacroScreen: View {
+    
+    @State private var vm = ObservableMacroViewModel()
+    
+    var body: some View {
+        Text(vm.title)
+            .task {
+               await vm.updateTitle()
+            }
+    }
+}
+
+#Preview {
+    ObservableMacroScreen()
+}
